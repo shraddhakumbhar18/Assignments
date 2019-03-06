c24bf2b37995271192ad735f7e5a20a5b224b7ca
Kumbhar <shraddha-karbhari.kumbhar@capgemini.com>
2019-03-06 09:43:04 +0530

    modified group.js

--- group.js
+++ group.js
@@ -5,3 +5,7 @@ function validateForm() {
       return false;
     }
   }
+  <form name="myForm" action="/action_page.php" onsubmit="return validateForm()" method="post">
+Name: <input type="text" name="fname">
+<input type="submit" value="Submit">
+</form>
